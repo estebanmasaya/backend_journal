8e409ba01ab7757a6b0e52df7d95d427454358b0
estebanmasaya <estebanmasaya@gmail.com>
2023-11-14 22:06:08 +0100

    funkar

--- src/main/java/com/kth/estmm/backend_journal/Persistence/PatientRepository.java
+++ src/main/java/com/kth/estmm/backend_journal/Persistence/PatientRepository.java
@@ -1,6 +1,8 @@
 package com.kth.estmm.backend_journal.Persistence;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.kth.estmm.backend_journal.BO.Patient;
 import com.kth.estmm.backend_journal.BO.Services.PatientFromJson;
 import com.kth.estmm.backend_journal.BO.User;
@@ -27,11 +29,55 @@ public class PatientRepository{
     }
 
     public void getPatient(long id) {
-        Mono<String> jsonResultMono = webClient.get().uri("/Patient?_id=" + id)
+        String jsonResult = webClient.get().uri("/Patient?_source=estamuelV2")
                 .retrieve()
-                .bodyToMono(String.class);
+                .bodyToMono(String.class).block();
 
-        jsonResultMono.subscribe(
+
+        System.out.println(jsonResult);
+        try {
+            JsonNode rootNode = objectMapper.readTree(jsonResult);
+            JsonNode entryNode = rootNode.path("entry");
+            System.out.println("A");
+            if(entryNode.isArray()){
+                System.out.println("B");
+                for(JsonNode j : entryNode){
+                    System.out.println("C");
+                    Long patientId = j.at("/resource/id").asLong();
+                    String name = j.at("/resource/name/0/given/0").asText() + " " +
+                            j.at("/resource/name/0/family").asText();
+                    String email = j.at("/resource/telecom/0").get("value").asText();
+                    System.out.println(patientId + " " + name + " " + email);
+                }
+
+            }
+
+/*
+
+            if(entry.isArray()){
+                for(JsonNode j : entry){
+                    JsonNode name = j.path("resource").path("name");
+                    if(name.isArray()){
+                        for(JsonNode s : name){
+                            System.out.println(s.path("family"));
+                            if(s.path("given").isArray()){
+                                for(JsonNode n : s.path("given")){
+                                    System.out.println(n);
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+            }
+
+*/
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+       /* jsonResultMono.subscribe(
                 jsonResult -> {
                     try {
                         JsonNode rootNode = objectMapper.readTree(jsonResult);
@@ -58,7 +104,7 @@ public class PatientRepository{
                 () -> {
                     System.out.println("Request completed");
                 }
-        );
+        );*/
 
     }
 
